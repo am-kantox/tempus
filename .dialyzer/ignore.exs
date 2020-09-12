28510f463c80e32241966f4f94758420e247fead
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2020-09-12 20:12:11 +0200

    Make dialyzer happy + deps update

--- .dialyzer/ignore.exs
+++ .dialyzer/ignore.exs
@@ -1,11 +1,6 @@
 [
-  {"lib/slot.ex", :call, 131},
-  {"lib/slot.ex", :call, 137},
-  {"lib/slots.ex", :invalid_contract, 95},
-  {"lib/tempus.ex", :no_return, 30},
-  {"lib/tempus.ex", :call, 31},
-  {"lib/tempus.ex", :call, 149},
-  {"lib/tempus.ex", :no_return, 266},
-  {"lib/tempus.ex", :no_return, 268},
-  {"lib/tempus.ex", :call, 272}
+  {"lib/slot.ex", :call, 163},
+  {"lib/slot.ex", :call, 169},
+  {"lib/tempus.ex", :no_return},
+  {"lib/tempus.ex", :call}
 ]
