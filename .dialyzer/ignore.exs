0762cf0bfe0c282809d364b7d38eef603e4deaa3
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2024-05-21 08:04:57 +0200

    Make dialyzer happy

--- .dialyzer/ignore.exs
+++ .dialyzer/ignore.exs
@@ -1,5 +1,6 @@
 [
   {"lib/tempus.ex", :no_return, 651},
-  {"lib/tempus/slots/stream.ex", :call, 430},
-  {"lib/tempus/slots/stream.ex", :extra_range, 386}
+  {"lib/tempus/slots/stream.ex", :extra_range, 389},
+  {"lib/tempus/slots/stream.ex", :call, 436},
+  {"lib/tempus/slots/stream.ex", :no_return, 443}
 ]
